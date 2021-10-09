7ad10d7d1eeb5b401c92ec3b44840ed9ac0204bf
bell-rabbit <andy19974a22@gmail.com>
2021-10-10 00:37:17 +0800

    feat(test) update the test app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-      <flipper_animation_generator :is-check-mode="false" ref="fag"/>
+      <flipper_animation_generator :is-check-mode="false" ref="fag" @completed="completed"/>
       <input value="bu" @click="$refs.fag.reload()"/>
+    <br>
+    <textarea ref="textarea"/>
   </div>
 </template>
 
@@ -12,7 +14,14 @@ export default {
   name: 'App',
   components: {
     flipper_animation_generator
-  }
+  },
+  methods: {
+    completed (e) {
+      this.$refs.textarea.value ='';
+      this.$refs.textarea.value = JSON.stringify(e);
+      console.log(e);
+    }
+  },
 }
 </script>
 
